ef4ebbd7502c0b245ff91e600537de6d1071d45b
Juan Cardozo <juaneze5games@gmail.com>
2025-02-07 07:07:58 -0300

    ok lo funcional de survey click

--- frontend/src/components/componentes-juanze/survey_click/survey_click.jsx
+++ frontend/src/components/componentes-juanze/survey_click/survey_click.jsx
@@ -10,8 +10,19 @@ const SurveyClick = ({ title, questions, tiempo, onClose }) => {
                     <div className='items__and__buttons_survey'>
                         <ol className='created__surveys_1'>
                             {questions.length > 0 ? (
-                                questions.map((q, index) => (
-                                    <li key={index} className='item__survey_1'>{q.title}</li>
+                                questions.map((question, index) => (
+                                    <li key={index} className='item__survey_1'>
+                                        <h3>{question.title}</h3>
+                                        <ul>
+                                            {question.options && question.options.length > 0 ? (
+                                                question.options.map((option, optionIndex) => (
+                                                    <li key={optionIndex}>{option.text}</li>
+                                                ))
+                                            ) : (
+                                                <li>No hay opciones disponibles</li>
+                                            )}
+                                        </ul>
+                                    </li>
                                 ))
                             ) : (
                                 <li className='item__survey_1'>No hay preguntas</li>
