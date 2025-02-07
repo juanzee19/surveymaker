7f83bcad4b75ff5d95d9a0bea6d52ebaad3b0370
Juan Cardozo <juaneze5games@gmail.com>
2025-02-07 12:37:47 -0300

    mejoro visual survey click

--- frontend/src/components/componentes-juanze/survey_click/survey_click.jsx
+++ frontend/src/components/componentes-juanze/survey_click/survey_click.jsx
@@ -6,15 +6,16 @@ const SurveyClick = ({ title, questions, tiempo, onClose }) => {
         <>
             <section className='box_survey'>
                 <div className='white__box__survey'>
-                    <h1>{title}</h1>
+                    
                     <div className='items__and__buttons_survey'>
+                    <h1>{title}</h1>
                       <p>*marque una casilla</p>
                       
                         <ol className='created__surveys_1'>
                             {questions.length > 0 ? (
                                 questions.map((question, index) => (
                                     <li key={index} className='item__survey_1'>
-                                        <h3>{question.title}</h3>
+                                        <h3>* {question.title} *</h3>
                                         <ul>
                                             {question.options && question.options.length > 0 ? (
                                                 question.options.map((option, optionIndex) => (
@@ -30,9 +31,10 @@ const SurveyClick = ({ title, questions, tiempo, onClose }) => {
                                 <li className='item__survey_1'>No hay preguntas</li>
                             )}
                         </ol>
-                    </div>
-                    <p>{tiempo}</p>
+                        <p>{tiempo}</p>
                     <button onClick={onClose} className="btn-back">Volver</button>
+                    </div>
+                    
                 </div>
             </section> 
         </>
